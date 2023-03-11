b31f9a1da9dbe8cd2e1cddfb29b323d62bc5f88d
kimSM94 <94KimSM@gmail.com>
2023-03-11 23:25:57 +0900

    20230311 리펙토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,16 +11,25 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+
+    // 리펙토링 CTRL + ALT + M
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     // 애플리케이션 환경 구성은 다 여기서
     // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
     // AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결) 해준다.
 
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 
 }
