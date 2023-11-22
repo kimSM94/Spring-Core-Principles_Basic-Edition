b5a798409be138afde339912b2bede9039de3350
kimSM94 <94KimSM@gmail.com>
2023-11-22 10:19:59 +0900

    애노테이션 @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,10 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
 
     private String url;
 
@@ -32,16 +33,17 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close :" + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception{
-        System.out.println("NetworkClient.afterPropertiesSet");
+
+    @PostConstruct
+    public void init() throws Exception{
+        System.out.println("NetworkClient.initi");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 
